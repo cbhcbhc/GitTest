0861909f8bd7084147c60629c05083c84f2a0229
cbhcbhc <2491221443@qq.com>
2022-12-19 10:48:11 +0800

    Initial commit

---
+++ src/test/java/com/springbootgittest/SpringBootGitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootgittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
